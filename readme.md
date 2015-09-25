5436b1d5db07b16e2115a6b8e1a47fb9facdea43
Jeff Logan <jeff@sixy.name>
2015-09-25 14:05:49 -0500

    Spoopifed the old extension

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 # sjw-to-liberal-mozilla
 
-Derived from the work of Steven Frank's [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) Chrome extension, danodemano's [cloud-to-butt-mozilla port](https://github.com/danodemano/cloud-to-butt-mozilla), and directhex's [sjw-to-liberal](https://github.com/directhex/cloud-to-butt/) Chrome extension.
+Derived from the work of Steven Frank's [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) Chrome extension, danodemano's [cloud-to-butt-mozilla port](https://github.com/danodemano/cloud-to-butt-mozilla), and Alex Hong's [sjw-to-skeleton](https://github.com/alexhong/sjw-to-skeleton) Chrome extension.
 
 
 ## Installing
 
-Download [sjw-to-liberal.xpi](https://github.com/Sixthhokage1/sjw-to-liberal-mozilla/raw/master/sjw-to-liberal.xpi)
+Download [sjw-to-skeleton.xpi](https://github.com/OpenJappix/sjw-to-skeleton-mozilla/raw/master/sjw-to-skeleton.xpi)
 
 
 ## Building
--- src/chrome/content/sjw-to-skeleton.js
+++ src/chrome/content/sjw-to-skeleton.js
@@ -1,19 +1,19 @@
 (function() {
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -28,17 +28,16 @@ function walk(node)
 			break;
 	}
 }
-    
+
 function handleText(textNode) {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/SJW/g, "liberal");
-	v = v.replace(/\bsjw\b/g, "liberal");
-	v = v.replace(/social justice warrior/g, "liberal");
-	v = v.replace(/Social Justice Warrior/g, "liberal");
-	v = v.replace(/\ban liberal\b/g, "a liberal");
-	v = v.replace(/\bliberal's\b/g, "liberals");
-	
+
+		v = v.replace(/\bsjw(s?)\b/ig, "skeleton$1");
+		v = v.replace(/\bsocjus\b/ig, "skeletonism");
+		v = v.replace(/\b(a)n (skeletons?)\b/ig, "$1 $2");
+		v = v.replace(/\b(s)ocial justice (warriors?)/ig, "$1keleton $2");
+
 	textNode.nodeValue = v;
 }
 
